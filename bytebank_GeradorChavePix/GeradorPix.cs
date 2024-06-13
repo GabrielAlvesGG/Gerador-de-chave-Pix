e2cc555e91d9fd65f2c6e00a0c0ec8913a6b1472
Gabriel Alves <alves.gabriel7@gmail.com>
2024-06-13 05:46:02 -0300

    Criação dos método de gerar pix com guid e documentação.

---
+++ bytebank_GeradorChavePix/GeradorPix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_GeradorChavePix
+{
+    /// <summary>
+    ///    Classe que gera chaves Pix usando o formato Guid.
+    /// </summary>
+    /// <returns></returns>
+    public static class GeradorPix
+    {
+        /// <summary>
+        /// Método que retorna uma chave aleatória PIX.
+        /// </summary>
+        /// <returns>Retorna uma chave PIX no formato string</returns>
+        public static string GetChavePix()
+        {
+            return Guid.NewGuid().ToString();
+        }
+        /// <summary>
+        /// Método que retorna uma lista aleatória de chaves PIX.
+        /// </summary>
+        /// <param name="numeroChaves">Quantidade de chaves a serem geradas</param>
+        /// <returns>Lista de strings de chaves Pix</returns>
+        public static List<string> GetChavesPix(int numeroChaves)
+        {
+            if (numeroChaves <= 0)
+                return null;
+            else
+            {
+                var chaves = new List<string>();
+
+                for (int i = 0; i < numeroChaves; i++)
+                {
+                    chaves.Add(Guid.NewGuid().ToString());
+                }
+
+                return chaves;
+            }    
+        }
+    }
+}
